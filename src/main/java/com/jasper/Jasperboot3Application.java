1dffc12253f4936f8701fe2aab271b1b82144962
Yusuke arakawa <yusu.arakawa@gmail.com>
2018-01-18 15:56:25 +0900

    First Commit

---
+++ src/main/java/com/jasper/Jasperboot3Application.java
@@ -0,0 +1,12 @@
+package com.jasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jasperboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jasperboot3Application.class, args);
+	}
+}
---
+++ src/main/java/com/jasper/pdfController.java
@@ -0,0 +1,51 @@
+package com.jasper;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+
+@RestController
+public class pdfController {
+
+	@Autowired
+    ApplicationContext context;
+    	
+	@GetMapping(path = "pdf/{jrxml}")
+	@ResponseBody
+    public void getPdf(@PathVariable String jrxml ,HttpServletResponse response) throws Exception {
+		//Get JRXML template from resources folder
+		Resource resource = context.getResource("classpath:jasperreports/"+jrxml+".jrxml");
+        //Compile to jasperReport
+        InputStream inputStream = resource.getInputStream();
+        JasperReport report=JasperCompileManager.compileReport(inputStream);		
+		//Parameters Set
+        Map<String, Object> params = new HashMap<>();
+        
+        //Data source Set
+        JRDataSource dataSource = new JREmptyDataSource();
+        //Make jasperPrint
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        //Media Type
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        //Export PDF Stream
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+    }
+}
